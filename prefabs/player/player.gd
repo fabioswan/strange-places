4d9b776704374fd9f1768573f46cc69f29f68ce8
adam <adam@codingchaos.com>
2026-02-16 14:42:22 -0600

    Connected walk animation to movement

--- prefabs/player/player.gd
+++ prefabs/player/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var WALK_SPEED = 200.0
@@ -17,12 +18,20 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_pressed("left"):
 		velocity.x = lerp(velocity.x, -WALK_SPEED, ACCEL_RATE * delta)
+		animated_sprite.flip_h = true
+		animated_sprite.play("walk")
 	elif Input.is_action_pressed("right"):
 		velocity.x = lerp(velocity.x, WALK_SPEED, ACCEL_RATE * delta)
+		animated_sprite.flip_h = false
+		animated_sprite.play("walk")
 	else:
 		velocity.x = lerp(velocity.x, 0.0, DECEL_RATE * delta)
+		animated_sprite.play("idle")
 		
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = -JUMP_PWR
+		
+	if not is_on_floor():
+		animated_sprite.play("idle")
 	
 	move_and_slide()
