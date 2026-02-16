ceb5839a0b289cd2c75c8fbffa578cfc32331edb
fabioswan <adam@codingchaos.com>
2026-02-15 18:04:50 -0600

    gravity

--- prefabs/player/player.gd
+++ prefabs/player/player.gd
@@ -1,5 +1,6 @@
-extends Area2D
+extends CharacterBody2D
 
+var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,5 +8,9 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	velocity.y += delta * GRAVITY
+	
+
+	var motion = velocity * delta
+	move_and_collide(motion)
